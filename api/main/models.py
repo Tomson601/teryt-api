954da72daeff00306f127ddec7ba936032d7a68b
Tomasz Jarko <tjarko@man.poznan.pl>
2022-12-30 12:03:14 +0100

    Updating Wojewodztwo model

--- api/main/models.py
+++ api/main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Wojewodztwo(models.Model):
     name = models.CharField(max_length=150, null=False)
     extra_name = models.CharField(max_length=150, null=False)
-    woj_id = models.CharField(max_length=150, null=False)
+    woj_id = models.CharField(max_length=150, null=False, unique=True)
     status_on_day =models.CharField(max_length=150, null=False)
 
     def __str__(self):
