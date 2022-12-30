cf46e81c8f508b18d13f7bad36ff2a2f5d6667f0
Tomasz Jarko <tjarko@man.poznan.pl>
2022-12-30 12:01:30 +0100

    Updating script

--- api/tools/scrap.py
+++ api/tools/scrap.py
@@ -1,6 +1,8 @@
 from zeep import Client
 from zeep.wsse.username import UsernameToken
 from datetime import datetime
+from main import models
+
 
 STATE_DATE = datetime.now()
 
@@ -18,10 +20,20 @@ client = Client(wsdl=CREDENTIALS['wsdl'], wsse=token)
 
 is_authenticated = client.service.CzyZalogowany()
 
+# Checking if auth is working properly
 if not is_authenticated:
     print("You are not logged in!")
 else:
     print("Login succeed")
 
 wojewodztwa = client.service.PobierzListeWojewodztw(STATE_DATE)
-print(wojewodztwa)
+
+# Creating objects in database
+for obj in wojewodztwa:
+    woj = models.Wojewodztwo.objects.create(
+        name = obj['NAZWA'],
+        extra_name = obj['NAZWA_DOD'],
+        woj_id = obj['WOJ'],
+        status_on_day = obj['STAN_NA']
+    )
+    print(f"Created: {woj.id}, {woj.name}")
